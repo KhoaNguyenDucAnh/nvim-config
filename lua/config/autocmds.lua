ff131f68a7eed11d7e73089c9c8903a65e71a8f4
Khoa Nguyen <robinkhoa06@gmail.com>
2026-01-28 22:06:42 -0500

    chore: update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*.pdf",
+  callback = function()
+    local file_path = vim.api.nvim_buf_get_name(0)
+    require("pdfview").open(file_path)
+  end,
+})
