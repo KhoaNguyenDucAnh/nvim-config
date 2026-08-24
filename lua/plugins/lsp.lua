a4612c571869060192295c7e0692a8771cc5dd74
Khoa <robinkhoa06@gmail.com>
2026-08-24 10:12:05 -0400

    update

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+-- local ruff = vim.g.lazyvim_python_ruff or "ruff"
+
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- ruff = {
+      --   cmd_env = { RUFF_TRACE = "messages" },
+      --   init_options = {
+      --     settings = {
+      --       logLevel = "error",
+      --     },
+      --   },
+      -- },
+      -- ruff_lsp = {},
+      basedpyright = {
+        settings = {
+          basedpyright = {
+            analysis = {
+              diagnosticSeverityOverrides = {
+                reportUnknownVariableType = "none",
+                reportUnknownMemberType = "none",
+                reportUnknownParameterType = "none",
+                reportUnknownArgumentType = "none",
+              },
+            },
+          },
+        },
+      },
+    },
+    -- setup = {
+    --   [ruff] = function()
+    --     Snacks.util.lsp.on({ name = ruff }, function(_, client)
+    --       -- Disable hover in favor of Pyright
+    --       client.server_capabilities.hoverProvider = false
+    --     end)
+    --   end,
+    -- },
+  },
+}
